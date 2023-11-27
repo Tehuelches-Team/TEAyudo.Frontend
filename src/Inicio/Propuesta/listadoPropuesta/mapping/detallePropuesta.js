4e2e2b3ebb814fc36cf51de62a3e9e5327cfb288
rocoide <roodrigo4@hotmail.com>
2023-11-27 12:05:28 -0300

    estilo de botones

--- src/Inicio/Propuesta/listadoPropuesta/mapping/detallePropuesta.js
+++ src/Inicio/Propuesta/listadoPropuesta/mapping/detallePropuesta.js
@@ -5,7 +5,7 @@ export default async function detallePropuesta(id, descripcion)
                 <h2>Detalles de la propuesta:  ${id}</h2>
                 <hr>
                 <p>Descripcion: ${descripcion} </p>
-                <button type="button" class="btn btn-danger" id="botonRechazar">Danger</button>
+                <button type="button" class="btn btn-danger" id="botonRechazar">rezhazar</button>
                 <button type="button" class="btn btn-primary" id="botonAbrirChat">Abrir chat</button>
                 <button type="button" class="btn btn-success" id="botonAceptar">Aceptar</button>
             </div>
